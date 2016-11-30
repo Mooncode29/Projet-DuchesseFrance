353efb7236980697955473067515d8862452ea5b
Juliane Blier <blier.juliane@gmail.com>
2016-11-30 09:28:08 +0100

    Ajout d'un exemple de données extraites de la SpreadSheets

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+var brutData = {
+	"range": "Sheet1!A1:D5",
+	"majorDimension": "ROWS",
+	"values": [
+		["Item", "Cost", "Stocked", "Ship Date"],
+		["Wheel", "$20.50", "4", "3/1/2016"],
+		["Door", "$15", "2", "3/15/2016"],
+		["Engine", "$100", "1", "30/20/2016"],
+		["Totals", "$135.5", "7", "3/20/2016"]
+	],
+}
