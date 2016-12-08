2dba97382c82efb3783ca808223f4c26370a0315
Juliane Blier <blier.juliane@gmail.com>
2016-12-08 11:22:51 +0100

    Retouche de la date à apposer sur le fichier

--- backup.js
+++ backup.js
@@ -1,8 +1,22 @@
 var fs = require('fs');
 
-var now = new Date();""
+var now = new Date();
+var year = now.getFullYear();
+var month = now.getMonth() + 1;
+var date = now.getDate();
+var minutes = now.getMinutes();
 
-var filename = __dirname + '/backup/' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + '-' + now.getMinutes() + '.json.bak'
+var filename = __dirname + '/backup/' + year + '-' + month + '-' + date + '-' + minutes + '.json.bak'
 
 fs.createReadStream(__dirname + '/public/marrainage.json').pipe(fs.createWriteStream(filename));
 
+fs.readdir(__dirname + '/backup', function(err, files){
+	if (err){
+		return;
+	}
+	if (files.length > 7){
+		files.forEach(function(f){
+			console.log('File : ' + f);
+		});
+	}
+});
