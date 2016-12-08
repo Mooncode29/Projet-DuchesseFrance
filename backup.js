bed1b29e2b0ffe4734e1fce9e65be7eed667c780
Cphil31 <cecile_philippe@live.fr>
2016-12-08 11:29:19 +0100

    Modif

--- backup.js
+++ backup.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var now = new Date();""
 
-var filename = __dirname + '/backup/' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + '-' + now.getMinutes() + '.json.bak'
+var filename = __dirname + '/backup/' + now.getFullYear() + '-' + now.getMonth() + '-' + now.getDay() + '-' +now.getHours()+ '-' + now.getMinutes() + '.json.bak'
 
 fs.createReadStream(__dirname + '/public/marrainage.json').pipe(fs.createWriteStream(filename));
 
