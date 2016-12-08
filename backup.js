479a037bf0dfb9dc7eaed754acbd1742ae115217
Juliane Blier <blier.juliane@gmail.com>
2016-12-08 12:29:47 +0100

    Système de Backup opérationnel

--- backup.js
+++ backup.js
@@ -6,21 +6,27 @@ var month = now.getMonth() + 1;
 var date = now.getDate();
 var minutes = now.getMinutes();
 
+var backupPath = __dirname + '/backup/';
 
-var filename = __dirname + '/backup/' + year + addZero(month) + addZero(date) + addZero(minutes) + '.json.bak'
+
+var filename = backupPath + year + addZero(month) + addZero(date) + addZero(minutes) + '.json.bak'
 
 
 fs.createReadStream(__dirname + '/public/marrainage.json').pipe(fs.createWriteStream(filename));
 
-fs.readdir(__dirname + '/backup', function(err, files){
+fs.readdir(backupPath, function(err, files){
 	if (err){
 		return;
 	}
 	if (files.length > 7){
-
-		files.forEach(function(f){
-			console.log('File : ' + f);
-		});
+		var older = files[0];
+		for (var i = 0 ; i < files.length ; i++){
+			if (files[i] < older){
+				older = files[i];
+			}
+		}
+		console.log(older);
+		fs.unlink(backupPath + older);
 	}
 });
 
