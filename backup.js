9316361d064b8df859c74ab30195316dd9171e8b
Cphil31 <cecile_philippe@live.fr>
2016-12-08 10:09:01 +0100

    simplification de la fonction creation

--- backup.js
+++ backup.js
@@ -1,59 +1,38 @@
 var fs = require('fs');
 // création des fichiers
 
-function read(operation){	
-	fs.readFile('public/marrainage.json','utf8',function(err,data){
-		if (err){
-			console.log(err);
-		}
-		operation(data)
-	});
+// function read(operation){	
+// 	fs.readFile('public/marrainage.json','utf8',function(err,data){
+// 		if (err){
+// 			console.log(err);
+// 		}
+// 		operation(data)
+// 	});
 
-} 
-function backup1(content){
-	fs.writeFile('backup/backup1_marrainage.json',content,'utf8', (err,data) => {
-		if (err) {
-			console.log("erreur");
-		}	
-	});
+// } 
+// function write(content){
+// 	fs.writeFile('backup/backup1_marrainage.json',content,'utf8', (err,data) => {
+// 		if (err) {
+// 			console.log("erreur");
+// 		}	
+// 	});
+// }
+// read(write);
+
+var seconde=1000;
+var minute = 60 * seconde;
+var heure = 60 * minute;
+var jour = 24 *heure ;
+
+var creer=fs.createReadStream('public/marrainage.json').pipe(fs.createWriteStream('backup/backup_marrainage.json'+1));
+
+function backup (){
+	setInterval(creer,jour);
+ // console.log(set);
+    
 }
+backup();
 
-function backUp2(content){
-	fs.writeFile('backup/backup2_marrainage.json',content,'utf8', (err,data) => {
-		if (err) {
-			console.log("erreur");
-		}	
-	});
-}
 
-function backUp3(content){
-	fs.writeFile('backup/backup3_marrainage.json',content,'utf8', (err,data) => {
-		if (err) {
-			console.log("erreur");
-		}	
-	});
-}
 
-function backUp4(content){
-	fs.writeFile('backup/backup4_marrainage.json',content,'utf8', (err,data) => {
-		if (err) {
-			console.log("erreur");
-		}	
-	});
-}
 
-function backUp5(content){
-	fs.writeFile('backup/backup5_marrainage.json',content,'utf8', (err,data) => {
-		if (err) {
-			console.log("erreur");
-		}	
-	});
-}
-
-;
-
-setInterval(read(backup1),10000);
-setInterval(read(backUp2),12000);
-setInterval(read(backUp3),13000);
-setInterval(read(backUp4),14000);
-setInterval(read(backUp5),15000);
